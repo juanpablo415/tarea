9aea2b8501ae2722dcf2da10fa58ef78a6e14e61
Juan Pablo Ccaccya Ayquipa <73971608+juanpablo415@users.noreply.github.com>
2021-10-26 10:20:27 -0500

    s2

--- mot.php
+++ mot.php
@@ -1,29 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>tablas de calculo</title>
+<meta charset="UTF-8">
 </head>
 <body>
     
-<center>
+
     <h1>tabla </h1>
     <form action="mot.php"method="GET">
         <input type="number" min="1"name="cajanumero" required>
-        <input type="sumit" value="crear tabla">
+        <input type="submit" value="crear tabla">
 
     </form>
     <?php
 
-    if(isset($_GET('cajanumero')))
+    if(isset($_GET['cajanumero']))
     {
         $numeroingresado = $_GET['cajanumero'];
         echo "<tabla border='1'><br>";
         echo "<h3>tabla del $numeroingresado</h3>";
-        for($i=1; $i<count($numeroingresado);>)
+        for($i=1; $i<=12; $i++)
+        {
+            $valor = $i*$numeroingresado;
+            echo "<tr>";
+            echo "<td>$i x $numeroingresado</td>";
+            echo "<td>$valor</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
     }
 
     ?>
-</center>
 </body>
 </html>
