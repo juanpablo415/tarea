83df76072ca370104b392eff0764bff4ade572f3
Juan Pablo Ccaccya Ayquipa <73971608+juanpablo415@users.noreply.github.com>
2021-10-26 11:32:35 -0500

    s4

--- calculadora.php
+++ calculadora.php
@@ -1,4 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    
+        <?php
+         
+         $m2 = $_GET['fila'];
+         $m3 = $_GET['columna'];
+         $m4 = $m2*$m3;
+         for ($i=0 ; $i < count($m))
 
 
-?>
+        ?>
+        
+
+    
+</body>
+</html>
--- mot.php
+++ mot.php
@@ -8,28 +8,50 @@
 
     <h1>tabla </h1>
     <form action="mot.php"method="GET">
-        <input type="number" min="1"name="cajanumero" required>
+        <input type="number" min="1"name="columna" required>
+        <input type="number" min="1"name="fila" required>
+        <input type="number" min="1"name="multiplicador" required>
         <input type="submit" value="crear tabla">
 
     </form>
-    <?php
+    
+<table width="100%" border="1">
+  <tr>
+  <?php
 
-    if(isset($_GET['cajanumero']))
+if(isset($_GET['multiplicador']))
+{
+    $m1 = $_GET['multiplicador'];
+    $m2 = $_GET['fila'];
+    $m3 = $_GET['columna'];
+
+    echo "<tabla border='1'><br>";
+    echo "<h3>tabla del $m1</h3>";
+
+    for($i=1; $i<=$m2; $i++)
     {
-        $numeroingresado = $_GET['cajanumero'];
-        echo "<tabla border='1'><br>";
-        echo "<h3>tabla del $numeroingresado</h3>";
-        for($i=1; $i<=12; $i++)
+        for($j=1; $j<=$m3; $j++)
         {
-            $valor = $i*$numeroingresado;
-            echo "<tr>";
-            echo "<td>$i x $numeroingresado</td>";
-            echo "<td>$valor</td>";
-            echo "</tr>";
+            echo "<td>";
+            $auxx=$i*$j;
+            echo "$i x $j=$auxx";
+            echo "<br>";
+            echo "</td>";
         }
-        echo "</table>";
+        echo "</tr>";
     }
+    echo "</table>";
+}
 
-    ?>
+?>
+    <td>
+
+    </td>
+    
+  </tr>
+  
+</table>
+
+        
 </body>
 </html>
